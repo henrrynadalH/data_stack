4ffea31b5ebe8b172edd8b7b967fa3f203803ea7
Henrry Lorenzo Nadal Cohuo <henrry@MacBook-Air-de-Henrry.local>
2024-02-16 11:33:34 -0600

    v 1.0.2

--- postgres_service/restore.sql
+++ postgres_service/restore.sql
@@ -22,7 +22,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
-DROP DATABASE dvdrental;
+--DROP DATABASE dvdrental;
 --
 -- Name: dvdrental; Type: DATABASE; Schema: -; Owner: postgres
 --
